44226f774f577bc1c5897f68300cf0ccd4e08eea
silveira.kaue@gmail.com <silveira.kaue@gmail.com@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2010-01-22 12:15:42 +0000

    Creating robopet project with shellscripts for checkout of the other projects

---
+++ checkout-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if test $# -ne 1
+then
+	echo "usage: $0 username"
+else
+	for projectname in ai communication gui radio simulation tracker
+	do
+		./checkout.sh $projectname $1
+	done
+fi
