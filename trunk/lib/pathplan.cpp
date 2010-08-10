49f4174ac881d13fa578fd59219077735e700b53
ninja.dalbem <ninja.dalbem@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2010-08-10 17:05:15 +0000

    pathplan is not yet ready

--- trunk/lib/pathplan.cpp
+++ trunk/lib/pathplan.cpp
@@ -5,7 +5,13 @@
 
 using namespace std;
 
+double DISTANCE(int num1, int num2)
+{
+    int num1x = REVERSE_INDEX_X(num1), num1y = REVERSE_INDEX_Y(num1); 
+    int num2x = REVERSE_INDEX_X(num2), num2y = REVERSE_INDEX_Y(num2);
 
+    return (double) sqrt(((num1x - num2x) * (num1x - num2x)) + ((num1y - num2y) * (num1y - num2y)));
+}
 
 Pathplan::Pathplan()
 {
@@ -99,3 +105,96 @@ void Pathplan::runPathplan( int pathplanIndex )
 			break;
 	}
 }
+
+bool Pathplan::aStar(int start, int goal)
+{
+	//conferir A* na wikipedia
+	
+	Closed.clear();
+	Open.clear();
+	Open.insert(start);
+
+    //cost[INDEX(6,6)] = 1000; //valor 1000 é um obstáculo
+
+	//cada um desses vetores tem o tamanho igual ao número de posições da matriz, ou seja, MAX_Y * MAX_X
+	g_score[start] = 0;
+	h_score[start] = DISTANCE(start, goal); //h_score: heurística (aproximação)
+											//DISTANCE: retorna a distância entre os dois pontos;
+											//utiliza a equação de distância entre dois pontos
+											//dist = ((x-x0)²+(y-y0)²)^(1/2)
+	f_score[start] = h_score[start]; //f_score: soma de g_score com h_score
+
+	for(int i=0; i<MAX_X * MAX_Y; i++)
+	    h_score[i] = DISTANCE(i, goal); //calcula a distância de todas as posições da matriz até o destino (goal)
+
+	while(!Open.empty())
+	{
+        ASet::iterator a = Open.begin(); //begin() retorna 
+		if( *a/*a->getElement()*/ == goal)
+		     return true;
+
+        int element = *a;//a->getElement();  //element é o elemento do início do iterador 'a'
+		//use env[x][y]
+		//field[element].setClosed(); //field[element] fica fechado pois é o início do caminho até o destino, 
+									//logo ele vai fazer parte do caminho final 
+		Closed.insert(*a); //insere a coordenada de início do caminho até o destino
+		Open.erase(*a); //essa coordenada agora não está mais aberta, pois foi analisada
+
+		for(int i = RIGHT; i < NUM_NEIGHBORS; i++) //i=0; i < 8; i++
+		{
+           //use env[x][y]
+           //int neighbor = element + corners[i]; //vizinho da posição 'element'
+
+		   //testa se o vizinho está dentro da grid ainda, pois ele pode estar fora. No caso de estar fora da 
+		   //grid esse vizinho é uma posição inválida	
+		   if( (IS_BOTTOM_BORDER(element) && (i == DOWN || i == LEFT_DOWN || i == RIGHT_DOWN)) ||
+		       (IS_UPPER_BORDER(element) && (i == UP || i == LEFT_UP || i == RIGHT_UP)) ||
+		       (IS_RIGHT_BORDER(element) && (i == RIGHT || i == RIGHT_UP || i == RIGHT_DOWN)) ||
+		       (IS_LEFT_BORDER(element) && (i == LEFT || i == LEFT_UP || i == LEFT_DOWN)) )
+		      continue;
+
+		   bool podePassar = true;
+           if(i>=RIGHT_DOWN) //i>=4
+               for(int j=RIGHT; j<RIGHT_DOWN; j++) //j=0; j<4; j++
+                   if(cost[element + corners[j]] == 1000) 
+                   { //se o vizinho (element+corners[j]) tiver um obstáculo, não pode passar
+                       podePassar = false;
+                       break;
+                   }
+
+           if(!podePassar)
+              continue; //termina a iteração vigente do for e começa outra
+
+		   if(field[neighbor].isClosed())
+		      continue; //se o vizinho está fechado, ou seja, já foi visitado e colocado no PAR, 
+						//então não pode continuar e termina a execução da atual iteração do for, começando uma nova
+
+		   double tentative_g_score = g_score[element] + DISTANCE(element, neighbor);
+		   //cout << "g_score[element] = " << g_score[element] << endl;
+		   //cout << "i = " << i << ", tentative_g_score = " << tentative_g_score << "\n";
+		   //getchar();
+		   
+		   bool tentative_is_better = false;
+
+		   if(field[neighbor].isNew())
+		   {
+		       field[neighbor].setOpen(); //se o vizinho for novo, então deixa ele disponível para ser analisado, 
+										  //como a operação membro 'setOpen()'
+		       tentative_is_better = true; 
+		   }
+		   else if(tentative_g_score < g_score[neighbor]) 
+		       tentative_is_better = true;
+
+		   if(tentative_is_better) //se a posição for a melhor, então ela é armazenada
+								   //no vetor backpointer
+		   {
+		       setBackpointer(neighbor, element); //neighbor é o índice de uma posição do campo no vetor backpointer
+		       g_score[neighbor] = tentative_g_score;
+		       f_score[neighbor] = g_score[neighbor] + h_score[neighbor] + cost[neighbor];
+		       Open.insert(Par(neighbor, f_score[neighbor]));
+		   }
+		}
+	}
+	return false;
+}
+
--- trunk/lib/pathplan.h
+++ trunk/lib/pathplan.h
@@ -2,13 +2,44 @@
 #define __ROBOPET_PATHPLAN_H__
 
 #include <vector>
-
 #include "point.h"
 #include "constants.h"
-
 #include "rrt.h"
+#include <iostream>
+#include <set>
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+//RLDU3791
+
+enum
+{
+	RIGHT, //0
+	LEFT,  //1
+	DOWN,  //2
+	UP,    //3
+	RIGHT_DOWN, //4
+	LEFT_UP,//5
+	RIGHT_UP, //6
+	LEFT_DOWN, //7
+	NUM_NEIGHBORS //8
+};
+
+inline bool IS_BOTTOM_BORDER(int num) { return ((int) num / MAX_Y == MAX_Y - 1) ? true : false; }
+inline bool IS_UPPER_BORDER(int num) { return ((int) num / MAX_Y == 0) ? true : false; }
+inline bool IS_RIGHT_BORDER(int num) { return ((int) num % MAX_X == MAX_X - 1) ? true : false; }
+inline bool IS_LEFT_BORDER(int num) { return ((int) num % MAX_X == 0) ? true : false; }
+
+inline int INDEX(int x, int y) { return x + (y * MAX_X); }
+inline int REVERSE_INDEX_X(int num) { return ((int) num % MAX_X); }
+inline int REVERSE_INDEX_Y(int num) { return ((int) num / MAX_Y); }
+
+double DISTANCE(int num1, int num2);
 
 
+typedef set<int> ASet;
 
 class Pathplan
 {
@@ -16,9 +47,14 @@ class Pathplan
 		list<state> pathFull;  //full solution, showing possible partial branches
 		list<state> pathFinal; //final solution, for path execution purposes
 
+		ASet Closed;
+		ASet Open;
+
 		int env[MAX_X][MAX_Y]; //environment matrix
-		
-		
+		int backpointer[MAX_X][MAX_Y];
+		double g_score[MAX_X * MAX_Y], h_score[MAX_X * MAX_Y], f_score[MAX_X * MAX_Y];
+		int cost[MAX_X * MAX_Y];
+
 		void fillEnv_playerBox(int centerx, int centery, int safetyCells);
 	
 		void runRRT(); //driver for RRT
@@ -33,18 +69,59 @@ class Pathplan
 		Pathplan();
 		Pathplan(Point initialpos, Point finalpos);
 		~Pathplan();
-		
-		
+				
 		Point getPathNode(int nodeIndex); //returns a specific node on pathFinal (initialState is 0)
-		
 		
 		void runPathplan( int pathplanIndex=PATHPLAN_RRT );
 		
 		void fillEnv(vector<Point> playersPositions); //fills the enviroment with positions of the obstacles (currently, only for players)
 
-
-
+		bool aStar(int start, int goal);
+		inline void setBackpointer(int index, int value) { backpointer[index] = value; }
+		inline int getBackpointer(int index) { return backpointer[index]; }
 };
 
 
 #endif
+
+
+#ifndef _GRID_H_
+#define _GRID_H_
+#define GRID_SIZE_X 5
+#define GRID_SIZE_Y 5
+
+enum flags
+{
+    OPEN_BIT = 1,
+    NEW_BIT = 2
+};
+
+typedef unsigned char byte;
+
+class Grid
+{
+    private:
+        byte tags; //Flags enumeradas acima
+
+        //byte cost;
+
+        byte sizeX; //tamanho da grade, para o sistema do RoboPET
+        byte sizeY;
+
+    public:
+        Grid(int sx = GRID_SIZE_X, int sy = GRID_SIZE_Y);
+        ~Grid(){ };
+
+        inline bool isOpen(){    return (tags & OPEN_BIT) && !(tags & NEW_BIT);    }  //está pronto para ser analisado
+        inline bool isClosed(){    return !(tags & OPEN_BIT) && !(tags & NEW_BIT);    } //já analisado
+        inline bool isNew(){    return (tags & NEW_BIT);    } //
+
+        inline void setOpen(){    tags = (tags | OPEN_BIT) - (tags & NEW_BIT);    }
+        inline void setClosed(){    tags = tags - (tags & OPEN_BIT) - (tags & NEW_BIT);    }
+
+        //inline void setCost(byte newCost){    cost = newCost;    }
+        //inline byte getCost(){    return cost;    }
+
+};
+#endif
+
