c3841deb9c67bd5d89785a89e100865d955b6ae7
ninja.dalbem <ninja.dalbem@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2010-11-04 19:37:01 +0000

    Whiped out Astar from Pathplan. Now, it's a generic pathplan class with virtual function Run(). Implemented instance of Rrt.

--- trunk/lib/rrt.h
+++ trunk/lib/rrt.h
@@ -1,11 +1,15 @@
 #ifndef _RRT_H_
 #define _RRT_H_
 
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 
 #include "point.h"
 #include "constants.h"
+#include "pathplan.h"
+
+using RP::Point;
+using namespace std;
 
 //Constantes Variáveis
 #define DIRECTIONS_TO_LOOK 3 //para extend, deve estar em {0, 1, ..., 8}
@@ -18,85 +22,63 @@
 #define STEPS_DELAY 7
 
 //constantes
-#define EMPTY_STATE (state(-10, -10))
+#define EMPTY_STATE (Point(-10, -10))
 #define SQR(x) ((x) * (x)) //para Distance
 
 
-using RP::Point;
-using namespace std;
+typedef list<Point> Solucao;
 
 
-enum{
-	LIVRE     = 0,
-	OBSTACULO = 1,
-	MARCADOR  = 2,
-	NODO      = 3,
-	CAMINHO   = 4
-	};
-
-
-//class state
-//{
-//    public:
-//        int x, y;
-//
-//        state(int a = -1, int b = -1) {
-//            x = a;
-//            y = b;
-//        }
-//
-//        bool operator!= (state param) {
-//            return x!=param.x || y!=param.y;
-//        }
-//        bool operator== (state param) {
-//            return x==param.x && y==param.y;
-//        }
-//
-//};
-
-
-typedef Point state;
-typedef list<state> Solucao;
-
+class Rrt: public Pathplan
+{
+	private:
+	
+	public:
+		Rrt() {}
+		~Rrt() {}
+			
+		void run();
+	
+};
 
 class RRTTree
 {
     private:
 
-		void treeToList_recursive(RRTTree *tree,list<state>*caminho);
+		void treeToList_recursive(RRTTree *tree,list<Point>*caminho);
 
     public:
 
-        state nodo;
+        Point nodo;
         list<RRTTree> filhos;
         RRTTree *pai;
 
         RRTTree() {pai=NULL;};
         ~RRTTree() {};
-        RRTTree(state param) { nodo = param; };
+        RRTTree(Point param) { nodo = param; };
 
-		list<state> treeToList(); //transforma rvore em uma lista de nodos
-		list<state> findSolucao(state goal); //cria uma lista de nodos com o caminho-solu��o
+		list<Point> treeToList(); //transforma arvore em uma lista de nodos
+		list<Point> findSolucao(Point goal); //cria uma lista de nodos com o caminho-solucao
 
-		RRTTree operator= (state param) { return nodo=param; };
+		RRTTree operator= (Point param) { return nodo=param; };
 };
 
 
 
-RRTTree* RRTPlan(int env[][MAX_Y], state initial, state goal); //função principal
-state ChooseTarget(state goal);
-RRTTree* Nearest(RRTTree *tree, state target);
-float Distance(state a, state b);
-state Extend(int env[][MAX_Y], state nearest, state target);
-void AddNode(RRTTree *nearest, state extended);
-state RandomState();
-int Collision(int env[][MAX_Y], state nearest, state extended); //há colisão numa linha entre os dois pontos?
-void nearestState(RRTTree *tree,state target,RRTTree **nearest);
+RRTTree* 	RRTPlan(envType env[][MAX_Y], Point initial, Point goal); //função principal
+Point 		ChooseTarget(Point goal);
+RRTTree* 	Nearest(RRTTree *tree, Point target);
+float 		Distance(Point a, Point b);
+Point 		Extend(envType env[][MAX_Y], Point nearest, Point target);
+void 		AddNode(RRTTree *nearest, Point extended);
+Point 		RandomState();
+int 		Collision(envType env[][MAX_Y], Point nearest, Point extended); //há colisão numa linha entre os dois pontos?
+void 		nearestState(RRTTree *tree,Point target,RRTTree **nearest);
 
 
 //funções de teste
-void printVarreTree(RRTTree *tree,int matrizPrint[][MAX_Y]);
-void print(RRTTree *tree,state initial,state goal,std::list<state> caminho,int env[][MAX_Y]);
+void 		printVarreTree(RRTTree *tree,int matrizPrint[][MAX_Y]);
+void 		print(RRTTree *tree,Point initial,Point goal,std::list<Point> caminho,envType env[][MAX_Y]);
 
 
 
