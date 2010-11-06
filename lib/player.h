5755886fbe11eabfcabb7cd0fe6ba56b45c28e68
ninja.dalbem <ninja.dalbem@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2010-11-06 14:51:33 +0000

    Refactoring Player.

--- lib/player.h
+++ lib/player.h
@@ -15,45 +15,49 @@ class Player: public MovingObject
     ~Player() {}
 
     //----- Getters -----
-    inline double getCurrentAngle() const { return _current_angle; }
-    inline double getFutureAngle() const { return _future_angle; }
-    inline double getDeltaAngle() const { return _delta_angle; }
+    inline double 	getCurrentAngle() const { return _current_angle; }
+    inline double 	getFutureAngle() const { return _future_angle; }
+    inline double 	getDeltaAngle() const { return _delta_angle; }
 
-    inline double getAngle() const { return getCurrentAngle(); }
+    inline double 	getAngle() const { return getCurrentAngle(); }
 
-    inline Role getRole() const { return _role; }
+    inline Role		getRole() const { return _role; }
 
-    inline double getDisplacementWithBall() const { return  _displacement_with_ball; }
+    inline double 	getDisplacementWithBall() const { return  _displacement_with_ball; }
 
-    inline int getDriblerIntensity() { return _dribler; }
-    inline int getKickerIntensity() { return _kicker; }
+    inline int 		getDriblerIntensity() { return _dribler; }
+    inline int 		getKickerIntensity() { return _kicker; }
 
-    inline int getId() { return _id; }
+    inline int 		getId() { return _id; }
 
     //----- Setters -----
-    void setCurrentAngle( double newCurrentAngle );
-    void setFutureAngle( double newFutureAngle );
-    void setDeltaAngle( double newDeltaAngle );
+    void 		setCurrentAngle( double newCurrentAngle );
+    void 		setFutureAngle( double newFutureAngle );
+    void 		setDeltaAngle( double newDeltaAngle );
 
     inline void setAngle(double a) { setCurrentAngle(a); } 
     
-    void setRole( Role newRole );
+    void 		setRole( Role newRole );
 
-    void setDisplacementWithBall( double newDisplacementWithBall ) ;
+    void 		setDisplacementWithBall( double newDisplacementWithBall ) ;
 
-    void setDriblerIntensity(int intensity);
-    void setKickerIntensity(int intensity);
+    void 		setDriblerIntensity(int intensity);
+    void 		setKickerIntensity(int intensity);
 
     void setId(int id);
 
     //----- Others -----
-    double calcDeltaAngle();
+    double 		calcDeltaAngle();
 
     inline bool isDribling() { return _dribler > 0; }
     inline bool isKicking() { return _kicker > 0; }
 
-	//stays in place
 	inline void stay() { setFuturePosition(getCurrentPosition()); }
+	void 		keepDistanceToBall(double distance);
+	void 		kick();
+	void 		pointTo( Point p );
+	bool 		isPointingTo( Point p );
+
 
     Player& operator=(const Player& other);
 
