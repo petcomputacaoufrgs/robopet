0abe9904a000849ab75bd3ee0c6ededf314bcc8b
lfzawacki <lfzawacki@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2011-01-10 14:11:20 +0000

    corrected error in binding generator

--- trunk/lib/userdata/robopet_lua.h
+++ trunk/lib/userdata/robopet_lua.h
@@ -16,10 +16,9 @@ void openLibs(lua_State* L)
 {
     luaopen_Point(L);
     luaopen_Vector(L);
+    luaopen_MovingObject(L);
     luaopen_Player(L);
     luaopen_Line(L);
-    luaopen_MovingObject(L);
     luaopen_PhysicsRobot(L);
     luaopen_Referee(L);
 }
-
