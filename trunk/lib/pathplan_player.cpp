9c0925213e66a0da68664157609e215f86d379a9
ninja.dalbem <ninja.dalbem@b2abf0a2-074e-11df-beaf-bb271b2086c6>
2011-08-02 21:47:17 +0000

    Commiting all robopet. working with ai pathplan

--- trunk/lib/pathplan_player.cpp
+++ trunk/lib/pathplan_player.cpp
@@ -2,7 +2,8 @@
 
 void PathplanPlayer::calculateFuturePosition()
 {
-	var++;
+	itercount++;
+	
 	// Case 1: no pathplanning has been calculated for this player.
 	if ( node == PATH_NOT_READY ) {
 		pathplan->setInitialPos( getPosition() );
@@ -10,19 +11,21 @@ void PathplanPlayer::calculateFuturePosition()
 		
 		cout << "running pathplan..." << endl;
 		pathplan->run();
-		var=0;
+		itercount=0;
 		
 		if(pathplan->status == SUCCESS)
 			node = 0;
+		else
+			setFuturePosition( getCurrentPosition() );
 	}
 	// Case 2: this player has reached it's final goal.
-	else if ( isAt( pathplan->getFinalPos(), 60 ) || var > 500 ) {
+	else if ( isAt( pathplan->getFinalPos(), 60 ) || itercount > 10 ) {
 		node = PATH_NOT_READY;
 		return;
 	}
 	// Case 3: this player has reached the destinated pathplan node.
 	else if ( isAt( getActualNode(), 60 ) ) {
-		node += 1;
+		++node;
 	}
 	
 	if(node != PATH_NOT_READY)
